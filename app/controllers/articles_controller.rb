dbab31be98c9f52184c9426c784208781d3e324e
miyuki-muneishi <kyoruni@gmail.com>
2019-07-30 19:28:33 +0900

    [Add]action destroy
    削除処理を追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,6 +13,12 @@ class ArticlesController < ApplicationController
     redirect_to :root
   end
 
+  def destroy
+    article = Article.find(params[:id])
+    article.destroy
+    redirect_to :root
+  end
+
   private
   def article_params
     # binding.pry
