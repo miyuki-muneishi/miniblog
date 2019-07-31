f74bb868e399e9c5477881c6e5654575d838fd84
miyuki-muneishi <kyoruni@gmail.com>
2019-07-31 14:18:16 +0900

    [Add]articles add user_id column
    articlesテーブルに、ユーザーidを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190731032618) do
+ActiveRecord::Schema.define(version: 20190731051718) do
 
   create_table "articles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "text",       limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
     t.string   "title"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
